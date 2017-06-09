0642bbb310bb4f2d180611c52944c7e31a9309b7
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-06-09 05:21:03 +0300

    Prettify

--- src/index.tsx
+++ src/index.tsx
@@ -17,4 +17,3 @@ render(
   </div>,
   app,
 );
-
--- stories/index.tsx
+++ stories/index.tsx
@@ -2,15 +2,14 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { UncontrolledZoomable } from 'components/Zoomable/UncontrolledZoomable';
 
-storiesOf(UncontrolledZoomable.name, module)
-  .add('with text', () =>
-    <div style={{ overflow: 'auto', height: 500 }}>
-      <UncontrolledZoomable minScale={1} maxScale={Infinity}>
-        <img
-          style={{ height: 250 }}
-          alt="Cephalometric radiograph"
-          src="https://upload.wikimedia.org/wikipedia/commons/6/66/Cephalometric_radiograph.JPG"
-        />
-      </UncontrolledZoomable>
-    </div>,
-  );
+storiesOf(UncontrolledZoomable.name, module).add('with text', () =>
+  <div style={{ overflow: 'auto', height: 500 }}>
+    <UncontrolledZoomable minScale={1} maxScale={Infinity}>
+      <img
+        style={{ height: 250 }}
+        alt="Cephalometric radiograph"
+        src="https://upload.wikimedia.org/wikipedia/commons/6/66/Cephalometric_radiograph.JPG"
+      />
+    </UncontrolledZoomable>
+  </div>,
+);
