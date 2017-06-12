bda823999d9d2c8270c4ca7c7c0f9570dbefb251
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-06-12 04:11:51 +0300

    Prettify [skip ci]

--- test/index.js
+++ test/index.js
@@ -2,7 +2,11 @@ const requireTestFile = require.context('./', true, /[^.d]\.tsx?$/);
 const testFiles = requireTestFile.keys();
 testFiles.forEach(requireTestFile);
 
-const requireSourceFile = require.context('../src', true, /[^index][^.d]\.tsx?$/);
+const requireSourceFile = require.context(
+  '../src',
+  true,
+  /[^index][^.d]\.tsx?$/,
+);
 const sourceFiles = requireSourceFile.keys();
 
 sourceFiles.forEach(requireSourceFile);
