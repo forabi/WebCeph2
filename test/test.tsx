149f4fafcb28ce1856f5edb1d294a18ee37adfa7
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-06-12 05:10:53 +0300

    Continue work on coverage

--- test/test.tsx
+++ test/test.tsx
@@ -1,7 +1,15 @@
+import * as React from 'react';
 import expect from 'expect';
+import Zoomable from 'components/Zoomable/Zoomable';
+import UncontrolledZoomable from 'components/Zoomable/UncontrolledZoomable';
+import noop from 'lodash/noop';
 
 it('Test 1', () => {
   expect(1).toEqual(1);
+  expect(
+    <Zoomable originX={0} originY={0} scaleFactor={1} onZoom={noop} />,
+  ).toExist();
+  expect(<UncontrolledZoomable />).toExist();
 });
 
 it('Test 2', () => {
--- webpack.config.js
+++ webpack.config.js
@@ -330,6 +330,7 @@ module.exports = {
         ]),
       },
 
+      // Code Coverage
       ifTest({
         enforce: 'post',
         test: /\.tsx?$/,
